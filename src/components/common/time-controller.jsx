641b5583f0f7980f61aad9cf7e23114bb272b2b5
azeriand <andrearomeracosta4@gmail.com>
2025-04-20 21:14:34 +0200

    create time-controller component

---
+++ src/components/common/time-controller.jsx
@@ -0,0 +1,14 @@
+import Card from './card'
+import Button from './button'
+
+export default function TimeController(){
+    return(
+        <Card>
+            <p></p>
+            <div>
+                <Button/>
+                <Button/>
+            </div>
+        </Card>
+    )
+}
