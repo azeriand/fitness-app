40856dfef9a4ae9e84c4c3f317e864f9b3a10bd3
azeriand <andrearomeracosta4@gmail.com>
2025-04-20 19:20:14 +0200

    adjust list-container and list-item components

--- src/components/common/list-container.jsx
+++ src/components/common/list-container.jsx
@@ -1,12 +1,34 @@
 import './list-container.css'
+import './list-item.css'
+import Card from './card.jsx'
 import SectionName from './section-name'
+import { useState } from 'react'
 
-export default function List({children, name}){
+export default function List({children, onListItemSelected, name, defaultValue, items, ...cardProps}){
+
+    const [listItemSelected, setListItemSelected] = useState(defaultValue ?? items[0]);
+
+    const onItemClicked = (item) => {
+        setListItemSelected(item);
+        onListItemSelected(item);
+        console.log('item clicked')
+    }
+
     return(
         <div>
              <SectionName section={name}/>
             <ul className='list'>
-                {children}
+                {items.map(item =>
+                    <>
+                        <Card noBlur appearance={item.destination === listItemSelected.destination ? 'mate' : 'ghost'} rounded='md' noPadding onClick={() => onItemClicked(item)} {...cardProps}>
+                            <div  className='list-item'>
+                                {item.logo}
+                                {item.label}
+                            </div>
+                        </Card>
+                        <hr/>
+                    </>
+                 )}
             </ul>
         </div>
     )
