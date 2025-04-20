f755a80d8f3fb662f3065bb6a92e6dc65b7e739b
azeriand <andrearomeracosta4@gmail.com>
2025-04-20 19:23:56 +0200

    adjust stats page

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,5 +1,19 @@
+import './stats.css'
+import Tab from "../components/common/tab";
+import Button from "../components/common/button"
+
 export default function Stats(){
+
+    const tabsItems = ['Last Trainings', 'Muscle Groups', 'Per Exercise'];
+
     return(
-        <h1>STATS</h1>
+        <>
+            <p className='page-name'>Stats</p>
+            <Tab items={tabsItems}/>
+            <div className='lastTrainingsFlex'>
+                <p>Last Trainings</p>
+                <Button label='132 Trainings'/>
+            </div>
+        </>
     )
 }
