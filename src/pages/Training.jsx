16cbd37dd73c7ee44f9be73f9f6bdf1cd96e1c68
azeriand <andrearomeracosta4@gmail.com>
2025-04-20 20:22:56 +0200

    add action buttons to training page

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -1,11 +1,18 @@
-import SectionName from "../components/common/section-name"
-import SetsWidget from "../components/sets-widget"
-import RowSet from "../components/common/row-set"
+import './training.css'
+import SectionName from '../components/common/section-name'
+import SetsWidget from '../components/sets-widget'
+import RowSet from '../components/common/row-set'
+import Button from '../components/common/button'
 
 export default function Training(){
     return(
         <>
             <p className='page-name'>Workout Started</p>
+            <div className='display-flex'>
+                <Button label='View Routine'/>
+                <Button label='Finish Routine' color='green'/>
+                <Button label='Discard Routine' color='red'/>
+            </div>
             <SectionName section='Exercises'/>
             <SetsWidget>
                 <RowSet/>
