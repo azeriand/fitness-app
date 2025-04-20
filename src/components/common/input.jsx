99fc628861703cef014162f42abd676b5f9b1103
azeriand <andrearomeracosta4@gmail.com>
2025-04-20 19:19:32 +0200

    adjust input component

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,16 +1,27 @@
 import './input.css'
 import Card from './card'
+import CardStyle from './card'
 
-export default function Input({type, value, disabled, placeholder, rounded, size, maxLength, onChange, ...cardProps}){
+export default function Input({type, value, disabled, placeholder, rounded, size, maxLength, onChange, iconPosition='left', icon, ...cardProps}){
     function inputUpdated(ev){
         onChange(ev.target.value)
+    }
+     
+    const inputStyle = {
+        ...cardProps.style,
+        display: 'flex',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 
     const classnames = `input ${cardProps.fullWidth ? 'full-width' : ''}`
     return(
     <div className={classnames}>
-        <Card noBlur noPadding rounded={rounded} {...cardProps}>
-            <input onChange={inputUpdated} placeholder={placeholder} value={value} disabled={disabled} type={type} size={size} maxLength={maxLength}></input>
+        <Card noBlur noPadding rounded={rounded} {...cardProps} style={inputStyle}>
+            <div className='left'>{iconPosition === 'left' && icon}</div>
+            <input onChange={inputUpdated} placeholder={placeholder} value={value} disabled={disabled} type={type} size={size} maxLength={maxLength} style={inputStyle}></input>
+            <div className='right'>{iconPosition === 'right' && icon}</div>
         </Card>
     </div>
     )
