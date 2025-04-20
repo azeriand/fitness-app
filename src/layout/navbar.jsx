b2d1e9e2f83954370ddd28cae5b717c9922f765a
azeriand <andrearomeracosta4@gmail.com>
2025-04-20 19:22:41 +0200

    adjust navbar component

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 import Card from '../components/common/card'
 import {FaDumbbell} from 'react-icons/fa'
@@ -6,26 +6,37 @@ import {FaChartSimple} from 'react-icons/fa6'
 import { IoMdSettings } from 'react-icons/io';
 import { TbHomeFilled } from 'react-icons/tb';
 import List from '../components/common/list-container';
-import ListItem from '../components/common/list-item';
-import CalendarView from '../components/common/calendar-view'
 import generateTrainingDays from '../data/generateTrainingDays';
+import SectionName from '../components/common/section-name';
+import StreakInfo from '../components/common/streak-info';
+import TrainingWidget from '../components/training-widget';
 import { useState } from 'react';
 
 export default function NavBar() {
     const navigate = useNavigate()
+    const location = useLocation();
+    const { pathname } = location;
+
     const [selectedDates] = useState(generateTrainingDays().map(({day}) => day));
     
+    const listItems = [
+        {logo: <TbHomeFilled/>, label: 'Home', destination: '/'},
+        {logo: <FaDumbbell/>, label: 'Train', destination: '/train'},
+        {logo: <FaChartSimple/>, label: 'Stats', destination: '/stats'},
+        {logo: <IoMdSettings/>, label: 'Settings', destination: '/settings'}
+    ]
+
+    const currentItem = listItems.find(item => item.destination === pathname);
+
     return (
         <Card intensity={500} noPadding>
-            <CalendarView selectedDates={selectedDates}/>
+            <StreakInfo/>
+            <SectionName section='streak'/>
+            {/* <CalendarView selectedDates={selectedDates}/> */}
             <div style={{padding: '1rem'}}>
-                <List name='List name'>
-                    <ListItem logo={<TbHomeFilled/>} label='Home' onItemClicked={() => navigate('/')}/>
-                    <ListItem logo={<FaDumbbell/>} label='Train' onItemClicked={() => navigate('/train')} />
-                    <ListItem logo ={<FaChartSimple/>} label='Stats' onItemClicked={() => navigate('/stats')}/>
-                    <ListItem logo ={<IoMdSettings/>} label='Settings' onItemClicked={() => navigate('/settings')}/>
-                </List>
+                <List name='List name' items={listItems} defaultValue={currentItem} onListItemSelected={(itemSelected) => navigate(itemSelected.destination)}/>
             </div>
+            <TrainingWidget/>
         </Card>
     )
 }
