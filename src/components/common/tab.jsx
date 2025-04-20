dc504b14ccc961652abbd7733098795b4bea8e5e
azeriand <andrearomeracosta4@gmail.com>
2025-04-20 19:21:26 +0200

    adjust tab component

--- src/components/common/tab.jsx
+++ src/components/common/tab.jsx
@@ -5,20 +5,21 @@ import Card from './card'
 import SectionName from './section-name'
 import Button from './button'
 
-export default function Tab({items, onTabSelected, ...cardProps}){
-    const [tabSelected, setTabSelected] = useState(null);
+export default function Tab({items, onTabSelected, defaultValue, ...cardProps}){
+    const [tabSelected, setTabSelected] = useState(defaultValue);
 
     const selectTab = (item) => {
         setTabSelected(item)
         onTabSelected(item)
     }
+    
     return(
         <>
             <SectionName section='Filter by'/>
-            <Card noBlur noPadding fitWidth rounded='md' {...cardProps}>
+            <Card noBlur noPadding fitWidth rounded='md' {...cardProps} color='blue'>
                 <div className='flex-buttons'>
                     {
-                        items.map(item => <Button onClick={() => selectTab(item)} key={item} appearance={item === tabSelected ? 'mate' : 'ghost'} label={item}/>)
+                        items.map(item => <Button onClick={() => selectTab(item)} color='blue' key={item} appearance={item === tabSelected ? 'mate' : 'ghost'} dark={item === tabSelected ? false : true } label={item}/>)
                     }
                 </div>
             </Card>
