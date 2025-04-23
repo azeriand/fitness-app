7d4fe935c16be0c3ee3b85f76f0640db4cfc9068
azeriand <andrearomeracosta4@gmail.com>
2025-04-23 10:41:55 +0200

    create time-controller component

--- src/components/common/time-controller.jsx
+++ src/components/common/time-controller.jsx
@@ -1,13 +1,18 @@
+import './time-controller.css'
 import Card from './card'
 import Button from './button'
+import { FaPause } from "react-icons/fa";
+import { FaStop } from "react-icons/fa";
 
 export default function TimeController(){
     return(
-        <Card>
-            <p></p>
-            <div>
-                <Button/>
-                <Button/>
+        <Card noPadding fitWidth>
+            <div className='flex'>
+                <div className='routine-time'>12:26</div>
+                <div className='stop-play'>
+                    <Button icon={<FaPause/>} appearance='ghost'/>
+                    <Button icon={<FaStop/>} appearance='mate'/>
+                </div>
             </div>
         </Card>
     )
