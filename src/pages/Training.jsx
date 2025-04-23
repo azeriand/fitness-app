f087ffc6285e3a94611361b7f456678fe2f7a4d9
azeriand <andrearomeracosta4@gmail.com>
2025-04-23 10:42:21 +0200

    add time-controller component to training page

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -3,15 +3,19 @@ import SectionName from '../components/common/section-name'
 import SetsWidget from '../components/sets-widget'
 import RowSet from '../components/common/row-set'
 import Button from '../components/common/button'
+import TimeController from '../components/common/time-controller'
 
 export default function Training(){
     return(
         <>
             <p className='page-name'>Workout Started</p>
-            <div className='display-flex'>
-                <Button label='View Routine'/>
-                <Button label='Finish Routine' color='green'/>
-                <Button label='Discard Routine' color='red'/>
+            <div className='display-flex between'>
+                <TimeController/>
+                <div className='display-flex'>
+                    <Button label='View Routine'/>
+                    <Button label='Finish Routine' color='green'/>
+                    <Button label='Discard Routine' color='red'/>
+                </div>
             </div>
             <SectionName section='Exercises'/>
             <SetsWidget>
