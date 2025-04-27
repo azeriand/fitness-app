f0abe7e9baf2863bc827e807dfffc0b365bfbb19
azeriand <andrearomeracosta4@gmail.com>
2025-04-27 19:32:50 +0200

    create exercise-card component

---
+++ src/components/exercise-card.jsx
@@ -0,0 +1,23 @@
+import './exercise-card.css'
+import Goku from '../assets/goku2.jpg'
+import Card from './common/card'
+import Avatar from './common/avatar'
+import Input from './common/input'
+import Badge from './common/badge'
+
+export default function ExerciseCard({exercises, label, badge, sets, ...cardProps}){
+
+    return(
+        <>
+            <div className='flex items-center gap-x-[1rem] w-full'>
+                <Avatar src={Goku} className='rounded-sm'/>
+                <div className='w-full'>
+                    <p className='m-0 p-0 text-start'>{label}</p>
+                    {badge}
+                </div>
+            </div> 
+            <Input style={{...cardProps.style}} maxLength='7' rounded='s' value= {sets} disabled/>
+
+        </>
+    )
+}
