aa2f05512b85382de43b634909f9c4e155a7b22a
azeriand <andrearomeracosta4@gmail.com>
2025-04-30 20:44:29 +0200

    fix input component

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -13,11 +13,11 @@ export default function Input({type, value, disabled, placeholder, size, maxLeng
 
     const classnames = `flex w-fit ${cardProps.fullWidth ? 'w-full' : ''}`
 
-    const inputClassName=`flex text-center justify-center items-center border-none bg-none py-[0.5rem] px-[1rem] m-0 ${className}`
+    const inputClassName=`flex text-center justify-center items-center border-none bg-none py-[0.5rem] px-[1rem] m-0 w-full ${className}`
 
     return(
     <div className={classnames}>
-        <Card className='flex text-center justify-center items-center rounded-md' noBlur noPadding {...cardProps} style={inputStyle}>
+        <Card className='flex text-center justify-center items-center rounded-md w-full' noBlur noPadding {...cardProps} style={inputStyle}>
             <div className='flex pl-[1rem] items-center'>{iconPosition === 'left' && icon}</div>
             <input className={inputClassName} onChange={inputUpdated} placeholder={placeholder} value={value} disabled={disabled} type={type} size={size} maxLength={maxLength} style={inputStyle}></input>
             <div className='flex pr-[1rem] items-center'>{iconPosition === 'right' && icon}</div>
