03e1831f28a695b756173910df18840bead049ce
andriugarcia <GVDrews@gmail.com>
2025-05-04 19:14:48 +0200

    fix: vertical scroll issues

--- src/components/filter-by-stats.jsx
+++ src/components/filter-by-stats.jsx
@@ -20,7 +20,7 @@ export default function FilterByStats(){
         return str.toLowerCase().replace(/[^A-Z0-9]/ig, "")
     }
 
-    const customScrollbar = "max-h-100 overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
+    const customScrollbar = "overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
 
     return(
 
