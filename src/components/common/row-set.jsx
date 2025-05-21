1c92966bd2fa53ffb90a66dcd74d86aa9c979f17
azeriand <andrearomeracosta4@gmail.com>
2025-05-21 12:33:20 +0200

    automate parameters

--- src/components/common/row-set.jsx
+++ src/components/common/row-set.jsx
@@ -2,12 +2,12 @@
 import Input from './input'
 import Checkbox from './checkbox'
 
-export default function RowSet(){
+export default function RowSet({num, reps, kg}){
     return(
         <>
-            <Input className='w-full' noPadding disabled placeholder='1'/>
-            <Input className='w-full' noPadding disabled placeholder='Previous'/>
-            <Input className='w-full' noPadding type='number'/>
+            <Input className='w-full' noPadding disabled placeholder={num}/>
+            <Input className='w-full' noPadding disabled placeholder={`${reps} x ${kg} Kg`}/>
+            <Input className='w-full' noPadding type='r'/>
             <Input className='w-full' noPadding type='number'/>
             <Checkbox/>
         </>
