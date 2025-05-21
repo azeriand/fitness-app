f3d53421fa7a177876b417b0adcc457c0cad6329
azeriand <andrearomeracosta4@gmail.com>
2025-05-21 12:33:38 +0200

    create timer

--- src/components/common/training-context.jsx
+++ src/components/common/training-context.jsx
@@ -1,14 +1,37 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 import generateTrainingDays from '../../data/generateTrainingDays'
 import exercises from '../../data/exercises.json'
+import routines from '../../data/routines.json'
 
 export const TrainingContext = createContext();
-export default function TrainingContextComponent({children}){
+export default function TrainingContextComponent({ children }) {
 
     const history = generateTrainingDays();
 
-    return(
-        <TrainingContext.Provider value={{history, exercises}}>
+    const [timer, setTimer] = useState(0)
+    const [timerformat, setTimerFormat] = useState("00:00:00")
+    const [trainingData, setTrainingData] = useState(routines[0])
+
+
+    function updateTimer() {
+        setTimer((oldValue) => {
+            let hours = Math.floor(oldValue/3600)
+            let minutes = Math.floor((oldValue%3600)/60)
+            let seconds = (oldValue%3600)%60
+            console.log(hours, minutes, seconds, oldValue)
+
+            const format = (time) => time<10 ? `0${time}` : `${time}`
+            setTimerFormat(`${format(hours)}:${format(minutes)}:${format(seconds)}`)
+
+            return oldValue + 1
+        })
+
+    }
+
+    useEffect(() => { setInterval(updateTimer, 1000) }, [])
+
+    return (
+        <TrainingContext.Provider value={{ history, exercises, routines, trainingData, setTrainingData, timer, timerformat}}>
             {children}
         </TrainingContext.Provider>
     )
