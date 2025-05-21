94b9e67d4214574a7cf521fbb6479dfe6ac54a77
azeriand <andrearomeracosta4@gmail.com>
2025-05-21 12:35:40 +0200

    add and use timer

--- src/components/common/time-controller.jsx
+++ src/components/common/time-controller.jsx
@@ -3,12 +3,17 @@ import Card from './card'
 import Button from './button'
 import { FaPause } from "react-icons/fa";
 import { FaStop } from "react-icons/fa";
+import { useContext } from 'react'
+import { TrainingContext } from './training-context'
 
 export default function TimeController(){
+    
+    const {timerformat} = useContext(TrainingContext)
+
     return(
         <Card className='w-fit' noPadding>
             <div className='flex items-center justify-between p-[0.5rem] gap-x-[7rem]'>
-                <div className='text-[1.5rem] font-bold'>12:26</div>
+                <div className='text-[1.5rem] font-bold'>{timerformat}</div>
                 <div className='flex items-center'>
                     <Button icon={<FaPause/>} appearance='ghost'/>
                     <Button icon={<FaStop/>} appearance='mate'/>
