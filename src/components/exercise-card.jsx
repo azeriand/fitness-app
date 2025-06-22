d2aca14768284cf287b30aebefb31f52f2e62f00
azeriand <andrearomeracosta4@gmail.com>
2025-06-22 20:03:12 +0200

    fix exercise-card component

--- src/components/exercise-card.jsx
+++ src/components/exercise-card.jsx
@@ -1,4 +1,4 @@
-import Goku from '../assets/goku2.jpg'
+import Card from './common/card'
 import Avatar from './common/avatar'
 import Input from './common/input'
 
@@ -6,13 +6,13 @@ export default function ExerciseCard({label, badge, sets, img, ...cardProps}){
     const inputClassNames = 'rounded-sm w-20 h-[2.5rem] text-base box-border p-0 text-center font-bold'
     return(
         <>
-            <div className='flex items-center gap-x-[1rem] w-full'>
+            <Card className='flex items-center gap-x-[1rem] w-full p-2' noPadding {...cardProps}>
                 <Avatar src={img} className='rounded-sm'/>
                 <div className='w-full'>
                     <p className='m-0 p-0 text-start'>{label}</p>
                     {badge}
                 </div>
-            </div> 
+            </Card> 
             {
                 sets && <Input centerText className={inputClassNames} style={cardProps.style} maxLength='7' value={sets} disabled/>
             }
