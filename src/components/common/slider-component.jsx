c97ab382cacf6cda1019da47e777931d2ff9e9af
azeriand <andrearomeracosta4@gmail.com>
2025-07-24 12:44:47 +0200

    create slider component

---
+++ src/components/common/slider-component.jsx
@@ -0,0 +1,8 @@
+import Slider from '@mui/material/Slider';
+import Card from './card.jsx';
+
+export default function SliderComponent(){
+    <Card noPadding appearance='ghost'>
+        <Slider disabled defaultValue={30} aria-label="Disabled slider" />
+    </Card>
+}
