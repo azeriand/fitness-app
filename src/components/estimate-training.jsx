17f79591e66e06d602dbbe99624e07e1bf403cdb
azeriand <andrearomeracosta4@gmail.com>
2025-07-24 12:45:07 +0200

    create and add estimate training component

--- src/components/estimate-training.jsx
+++ src/components/estimate-training.jsx
@@ -6,6 +6,12 @@ export default function EstimateTraining() {
     return(
         <Card noPadding appearance='ghost'>
             <SectionName section='estimate training' className='pb-[0.5rem] tracking-normal'/>
+            <div>
+                <SectionName section='max weight per rep' className='pb-[0.5rem] tracking-normal'/>
+                <SliderComponent/> 
+                <SectionName section='max reps per weight' className='pb-[0.5rem] tracking-normal'/>
+                <SliderComponent/>
+            </div>
         </Card>
     )
 }
