ab4125aae57c2a6227447df981a0e9b240e0fa80
azeriand <andrearomeracosta4@gmail.com>
2025-07-28 23:03:20 +0200

    add logic to estimate-training component

--- src/components/estimate-training.jsx
+++ src/components/estimate-training.jsx
@@ -1,12 +1,33 @@
 import Card from "./common/card";
 import SectionName from "./common/section-name";
 import SliderSpecific from "./slider-specific";
+import { useContext, useEffect } from "react";
+import { TrainingContext } from "./common/training-context";
+import { ExerciseContext } from "./exercise-context";
 
 export default function EstimateTraining() {
+
+    const { history } = useContext(TrainingContext);
+    const { filterSelected } = useContext(ExerciseContext);
 
     const duplicateSliderValue = (value) => {
         return value * 2
     };
+
+    //1RM = Peso levantado / [1.0278 - (0.0278 * número de repeticiones)] 
+    function getRM() {
+        let exerciseRM = [];
+        console.log('Filter selected:', filterSelected);
+        if (filterSelected && filterSelected.type === 'exercise') {
+            const dayFound = history.find((day) => day.exercises.find((exercise) => exercise.exercise_name === filterSelected.name) !== null)
+            const exerciseFound = dayFound.exercises.find((exercise) => exercise.exercise_name === filterSelected.name);
+            console.log({exerciseFound})
+            exerciseFound.sets.forEach((set) => { exerciseRM.push(set.weight / (1.0278 - (0.0278 * set.reps)))})
+        }
+        console.log(exerciseRM.reduce((prev, curr) => prev + curr, 0) / exerciseRM.length);
+    }
+
+    useEffect(getRM, [filterSelected])
 
     return(
         <Card noPadding appearance='ghost'>
