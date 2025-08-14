5d37c47b575d13a03b35d8e65fe328a65be72730
azeriand <andrearomeracosta4@gmail.com>
2025-08-14 19:17:56 +0200

    feat: add states

--- src/components/settings-context.jsx
+++ src/components/settings-context.jsx
@@ -3,8 +3,11 @@ import { createContext, useState, useEffect } from "react";
 export const SettingsContext = createContext();
 export default function SettingsContextComponent({children}){
 
+    const [defaultStreak, setDefaultStreak] = useState(3);
+    const [currentStreak, setCurrentStreak] = useState(0);
+    
     return(
-        <SettingsContext.Provider value={{}}>
+        <SettingsContext.Provider value={{defaultStreak, setDefaultStreak}}>
             {children}
         </SettingsContext.Provider>
     )
