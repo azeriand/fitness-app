3f74fd05013725dbeab70f06251564dfc6ffed93
azeriand <andrearomeracosta4@gmail.com>
2025-08-16 16:25:20 +0200

    create provisional streak configuration

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,25 @@
+import { Card } from 'azeriand-library'
+import Input from '../components/common/input'
+import { useContext } from 'react'
+import { SettingsContext } from '../components/settings-context'
+
+
 export default function Settings(){
+
+    const {defaultStreak, setDefaultStreak} = useContext(SettingsContext)
+
+    function handleChange(event) {
+        setDefaultStreak(event.target.value)
+    }
+
     return(
-        <h1>SETTINGS</h1>
+        <>
+            <h1>SETTINGS</h1>
+            <Card>
+                <p>Set the default streak value:</p>
+                <Input value={defaultStreak} onChange={handleChange}/>
+            </Card>
+
+        </>
     )
 }
