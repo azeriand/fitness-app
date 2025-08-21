ee038cd39c713d70548d76a610710bf2664e3e86
azeriand <andrearomeracosta4@gmail.com>
2025-08-21 13:16:52 +0200

    translate the comments to english

--- src/components/settings-context.jsx
+++ src/components/settings-context.jsx
@@ -13,14 +13,16 @@ export default function SettingsContextComponent({children}){
     const [currentStreak, setCurrentStreak] = useState(0);
     
     function calculateCurrentStreak(){
-        //Guardar los días del historial en un array
+        //Add the history days into an array
         let days = history.map((routine) => routine.day)
         let daysReversed = [...days].reverse();
         console.log("days", daysReversed)
-        //Transformar el array en dias de la semana (0-6)
+
+        //Transform the array elements into days of the week (0-6)
         let daysOfWeek = daysReversed.map((day) => dayjs(day).day());
         console.log("daysOfWeek", daysOfWeek)
-        //Crear un bucle que recorra los días, sumando 1 a un contador si el día es menor o igual que el siguiente (si cuenta el defaultStreak, se añade +1 al streak), si es mayor se resetea el contador
+
+        //Create a loop that goes through the days, +1 to a counter if the day is less than or equal to the next (if it reach the defaultStreak, +1 is added to the streak), if it's greater, reset the counter
         let dayCounter = 0;
         let streakCounter = 0;
         let streakActive = false;
@@ -29,26 +31,26 @@ export default function SettingsContextComponent({children}){
             const actualDay = daysOfWeek[i];
             const nextDay = daysOfWeek[i + 1];
 
-            //Si hoy es menor o igual que el siguiente día y no es el final del array, sumamos 1 al contador
+            //If today is less than or equal to the next day and it's not the end of the array, +1 to the counter
             if (nextDay != undefined && actualDay <= nextDay) {
                 dayCounter++;
 
-                // Si el contador es mayor o igual que el defaultStreak y no está activo el streak, suma uno al streak y se activa
+                //If the counter is greater than or equal to the defaultStreak and the streak is not active, +1 to the streak and activate it
                 if (dayCounter >= defaultStreak && !streakActive) {
                     streakCounter++;
                     streakActive = true;
                     setCurrentStreak(streakCounter)
                 }
             }
-            //Si es el final del array u hoy es mayor que el siguiente día hay dos opciones:
+            //If it's the end of the array or today is greater than the next day, we have two options:
             if (nextDay === undefined || actualDay > nextDay) {
-                //Si el streak está activo, se resetea el contador y se desactiva el streak
+                //If the streak is active, reset the counters
                 if (!streakActive) {
                     dayCounter = 0;
                     streakCounter = 0;
                     setCurrentStreak(0);
                 }
-                //Si el streak no está activo, se resetea el contador y se desactiva
+                //If the streak is not active, reset the counter and deactivate the streak
                 else {
                     dayCounter = 0;
                     streakActive = false;
@@ -56,10 +58,6 @@ export default function SettingsContextComponent({children}){
             }
             console.log("dayCounter", dayCounter, "streakCounter", streakCounter)
         }
-        // Crear un contador que sume 1 si la semana cumple el requisito y se resetea si no lo cumple
-        // Si la semana actual iguala o supera el defaultStreak, se suma 1 al currentStreak
-
-        //return el contador de currentStreak
     }
     
     useEffect(calculateCurrentStreak,[defaultStreak])
