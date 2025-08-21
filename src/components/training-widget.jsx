d4cc0c45f75568425dedfb886e770c1541e0b195
azeriand <andrearomeracosta4@gmail.com>
2025-08-21 18:16:23 +0200

    feat: redesign training widget

--- src/components/training-widget.jsx
+++ src/components/training-widget.jsx
@@ -1,13 +1,16 @@
 import { useNavigate } from 'react-router-dom';
-import { FaChevronLeft } from "react-icons/fa";
-import { FaChevronRight } from "react-icons/fa";
 import { FaPause } from "react-icons/fa";
 import { FaStop } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa";
 import { Button } from 'azeriand-library'
 import { Card } from 'azeriand-library'
-import Input from './common/input'
+import { useContext } from 'react';
+import { TrainingContext } from './common/training-context.jsx';
 
 export default function TrainingWidget({...cardProps}){
+
+    const {timerformat, startTraining, stopTraining, switchTimer} = useContext(TrainingContext);
+
     const navigate = useNavigate()
     const trainingWidgetProps = {
         padding: '0.5rem'
@@ -16,29 +19,19 @@ export default function TrainingWidget({...cardProps}){
     const inputClassNames = 'rounded-sm w-[2.5rem] h-[2.5rem] text-base box-border !px-0 text-center font-bold'
 
     return(
-        <Card noBlur intensity={500} {...cardProps} style={{...cardProps.style, ...trainingWidgetProps}}>
-            <div className='flex items-center justify-between'>
-                <div className='font-bold text-[1.5rem]'>12:26</div>
+        <>
+            <Card noBlur intensity={500} {...cardProps} style={{...cardProps.style, ...trainingWidgetProps}} className='flex items-center gap-x-[1rem] justify-between m-[0.5rem]'>
+                <div className='font-bold text-[1.5rem] w-fit'>{timerformat}</div>
                 <div className='flex items-center'>
-                    <Button icon={<FaPause/>} appearance='ghost'/>
-                    <Button icon={<FaStop/>} color='mate'/>
+                    <Button icon={<FaPause/>} appearance='ghost' onClick={switchTimer}/>
+                    <Button icon={<FaStop/>} appearance='mate' onClick={stopTraining}/>
                 </div>
+                <Button label='View More' className='w-full' onClick={()=> navigate('/training')}/>
+            </Card>
 
-            </div>
-            <div className='flex items-center justify-between py-[0.5rem]'>
-                <div className='truncate text-[1.1rem] font-bold justify-center'>Bench Press</div>
-                <div className='flex items-center gap-x-[0.25rem]'>
-                    <Button icon={<FaChevronLeft/>} appearance='mate' size='sm'/>
-                    <Input className={inputClassNames} maxLength='3' style={cardProps.style}/>
-                    <Input className={inputClassNames} maxLength='3' style={cardProps.style}/>
-                    <Button icon={<FaChevronRight/>} appearance='mate' size='sm'/>
-                </div>
-            </div>
-            <div className='flex items-center justify-around gap-x-[0.5rem]'>
-                <Button label='Open' className='w-full' onClick={()=> navigate('/training')}/>
-                <Button label='Finish' color='green' intensity='600' className='w-full'/>
-                <Button label='Discard' color='red' intensity='600' className='w-full'/>
-            </div>
-        </Card>
+            <Card noPadding noBlur className='m-[0.5rem]'>
+                <Button label='Start Training' icon={<FaPlay/>} position='right' className='w-full' onClick={()=> navigate('/routines')}/>
+            </Card>
+        </>
     )
 }
