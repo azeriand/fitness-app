86d98351223130b1b35d09d827cdc89c708ab859
azeriand <andrearomeracosta4@gmail.com>
2025-09-01 21:41:58 +0200

    add functions to LocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,20 +1,31 @@
 import { useState, useEffect } from "react";
 
-export default function useLocalStorage(key, defaultValue){
+export default function useLocalStorage(key, defaultValue) {
+    const [value, setValue] = useState(() => {
+        const localStorageValue = localStorage.getItem(key);
+        return localStorageValue !== null ? JSON.parse(localStorageValue) : defaultValue;
+    });
 
-    const [value, setValue] = useState(defaultValue)
+    const modifyCache = (newValue) => {
+        setValue((prevValue) => {
+            const valueToStore =
+                typeof newValue === "function" ? newValue(prevValue) : newValue;
+            localStorage.setItem(key, JSON.stringify(valueToStore));
+            return valueToStore;
+        });
+    };
 
-    const modifyCache = (newValue) => {     
-        setValue(newValue);
-        localStorage.setItem(key, JSON.stringify(newValue))
+    const removeState = () => {
+        setValue(defaultValue);
+        localStorage.removeItem(key);
     }
-    
-    useEffect(() => {
-        const localStorageValue = localStorage.getItem(key)
-        if (localStorageValue !== null) {
-            setValue(JSON.parse(localStorageValue))
-        }
-    }, [])
 
-    return [value, modifyCache]
+    useEffect(() => {
+        const localStorageValue = localStorage.getItem(key);
+        if (localStorageValue !== null) {
+            setValue(JSON.parse(localStorageValue));
+        }
+    }, [key]);
+
+    return [value, modifyCache, removeState];
 }
