54085537d0272724092d31cb3e53bc7e07bd50c6
azeriand <andrearomeracosta4@gmail.com>
2025-10-08 15:28:04 +0200

    adjust and style time-controller component

--- src/components/time-controller.jsx
+++ src/components/time-controller.jsx
@@ -1,34 +1,18 @@
 
 import { Card } from 'azeriand-library'
-import { Button } from 'azeriand-library'
-import { FaPause } from "react-icons/fa";
-import { FaPlay } from "react-icons/fa";
-import { FaStop } from "react-icons/fa";
 import { useContext } from 'react'
 import { TrainingContext } from './training-context'
+import PlayPause from './play-pause';
 
-export default function TimeController(){
+export default function TimeController({appearance}){
     
-    const {timerformat, switchTimer, resetTimer, trainingData} = useContext(TrainingContext)
-
-    const discard = () => {
-        switchTimer()
-        window.location.href = '/routines'
-    }
-
-    const renderIcon = () => {
-        if (trainingData.state === 'RUNNING') return <FaPause/>
-        else {return <FaPlay/>}
-    }
+    const {timerformat} = useContext(TrainingContext)
 
     return(
-        <Card className='w-fit' noPadding>
-            <div className='flex items-center justify-between p-[0.5rem] gap-x-[7rem]'>
+        <Card className='w-fit' noPadding appearance={appearance}>
+            <div className='flex items-center justify-between p-[0.5rem] gap-x-[0.5rem]'>
                 <div className='text-[1.5rem] font-bold'>{timerformat}</div>
-                <div className='flex items-center'>
-                    <Button icon={renderIcon()} appearance='ghost' onClick={switchTimer}/>
-                    <Button icon={<FaStop/>} appearance='mate' onClick={discard}/>
-                </div>
+                <PlayPause/>
             </div>
         </Card>
     )
