73bcac4714b621c6bfd6b94efea587aeaf7b7438
azeriand <andrearomeracosta4@gmail.com>
2025-10-09 10:47:23 +0200

    style: align top page buttons

--- src/layout/top-bar.jsx
+++ src/layout/top-bar.jsx
@@ -9,8 +9,8 @@ export default function TopBar(){
     const {theme, setTheme} = useContext(ThemeContext);
 
     return(
-        <Card className='w-full' appearance='ghost' noPadding>
-            <Button label='Go to portfolio!'/>
+        <Card className='w-full flex justify-end gap-[1rem] mb-[1rem] items-center' appearance='ghost' noPadding>
+            <Button label='Go to my portfolio!' className='h-full'/>
             <Button icon={theme === 'dark' ? <MdLightMode/> : <MdDarkMode/>} onClick={() => setTheme((oldValue) => oldValue === 'dark' ? 'light' : 'dark')}/>
         </Card>
     )
