aacc524e1178a9263a55c368557502a65d4ca683
azeriand <andrearomeracosta4@gmail.com>
2025-12-17 12:25:09 +0100

    style: make the background blur

--- src/components/popup-discard-finish.jsx
+++ src/components/popup-discard-finish.jsx
@@ -2,7 +2,7 @@ import { Card, Button } from 'azeriand-library';
 
 export default function PopupDiscardFinish({ onDiscard, onFinish, onCancel}) {
     return(
-        <Card className='w-[90%] h-fit max-w-[25rem] p-6 flex flex-col gap-y-6'>
+        <Card className='w-[90%] h-fit max-w-[25rem] p-6 flex flex-col gap-y-6' appearance='mate'>
             <p className='text-[1.25rem] font-semibold m-0'>Are you sure you want to discard this routine?</p>
             <div className='flex justify-center gap-x-4'>
                 <Button label='Cancel' onClick={onCancel}/>
