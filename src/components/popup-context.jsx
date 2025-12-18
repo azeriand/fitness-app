c1916dcb43ced57985e8f337010ea5f024493d52
azeriand <andrearomeracosta4@gmail.com>
2025-12-18 11:00:08 +0100

    define default popup value

--- src/components/popup-context.jsx
+++ src/components/popup-context.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from 'react';
 const PopupContext = createContext(null);
 
 export function PopupContextComponent({ children }) {
-    const [isPopupOpen, setIsPopupOpen] = useState(true);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
 
     return (
         <PopupContext.Provider value={{ isPopupOpen, setIsPopupOpen }}>
