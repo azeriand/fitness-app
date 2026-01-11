807b6d865d15616fac4e19f5a1fb99e5d9f78be4
andriugarcia <GVDrews@gmail.com>
2026-01-11 19:24:11 +0100

    fix: glow

--- src/components/training-widget.jsx
+++ src/components/training-widget.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { FaPlay } from "react-icons/fa";
-import { Button, GradientGlow } from 'azeriand-library'
+import { Button } from 'azeriand-library'
 import { Card } from 'azeriand-library'
 import TimeController from './time-controller.jsx';
 import { useContext } from 'react';
@@ -20,17 +20,7 @@ export default function TrainingWidget({...cardProps}){
         if (trainingData.state ==='STOPPED' || trainingData.state === null ){
             return (
                 <Card noPadding noBlur className='m-[0.5rem] py-[0.5rem]' appearance='ghost' {...cardProps} style={{...cardProps.style, ...trainingWidgetProps}}>
-                    <GradientGlow animation="pulse"
-                                  animationType="hover"
-                                  gradientTheme="purple-blue"
-                                  duration={0.6}
-                                  blurIntensity={15}
-                                  glowSpread={8}
-                                  opacity={0.9} 
-                                  easing='ease-in-out'
-                                  borderRadius="12px">
-                        <Button appearance="mate" color='purple' intensity={900} label='Start Training' icon={<FaPlay/>} position='right' className='w-full' onClick={()=> navigate('/routines')}/>
-                    </GradientGlow>
+                    <Button appearance="mate" color='purple' intensity={900} label='Start Training' icon={<FaPlay/>} position='right' className='w-full' onClick={()=> navigate('/routines')}/>
                 </Card>            
             )
         }
